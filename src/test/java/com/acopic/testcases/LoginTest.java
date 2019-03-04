7fe14a099eeb76441d49df2cd4cc27f4dfdd86b0
Antonio Copic <acopic.ext@autoscout24.com>
2019-03-04 23:59:50 +0100

    simple change

--- src/test/java/com/acopic/testcases/LoginTest.java
+++ src/test/java/com/acopic/testcases/LoginTest.java
@@ -9,6 +9,6 @@ public class LoginTest extends TestBase {
     @Test
     public void loginAsBankManager() throws InterruptedException {
         driver.findElement(By.cssSelector(OR.getProperty("bmlBtn"))).click();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
     }
 }
